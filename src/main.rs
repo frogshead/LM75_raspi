b27b5613e39c54b81fc56f6d31213833561d5bf8
Mikko Viitamäki <mikko.viitamaki@gmail.com>
2019-03-13 07:16:23 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+extern crate linux_embedded_hal as hal;
+extern crate lm75;
+
+use hal::I2cdev;
+use lm75::{ Lm75, SlaveAddr };
+fn main(){
+let dev = I2cdev::new("/dev/i2c-1").unwrap();
+let address = SlaveAddr::default();
+let mut sensor = Lm75::new(dev, address);
+let temperature = sensor.read_temperature().unwrap();
+println!("Temperature:  {}", temperature);
+}
