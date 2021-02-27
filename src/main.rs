16207a5978527f97aff9a82dd0c04bb8a829e0fe
Mikko Viitamäki <mikko.viitamaki@gmail.com>
2021-02-27 12:01:58 +0200

    Write values to csv file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
 extern crate linux_embedded_hal as hal;
 extern crate lm75;
 
+use std::fs::OpenOptions;
+
+use chrono::prelude::*;
+use csv::WriterBuilder;
+use serde::Serialize;
+
+
+#[derive(Debug, Serialize)]
+struct Temperature {
+    temperature: f32,
+    date_time: String
+
+}
 use hal::I2cdev;
 use lm75::{ Lm75, SlaveAddr };
 fn main(){
@@ -9,4 +22,9 @@ let address = SlaveAddr::default();
 let mut sensor = Lm75::new(dev, address);
 let temperature = sensor.read_temperature().unwrap();
 println!("Temperature:  {}", temperature);
+let file = OpenOptions::new().write(true).append(true).open("/home/mikko/temperatures.csv").unwrap();
+    let mut writer = WriterBuilder::new().has_headers(false).from_writer(file);// Writer::from_writer(file);
+    let dt = Local::now().to_string();
+    let temp = Temperature{ temperature: temperature, date_time: dt};
+    writer.serialize(temp).unwrap();
 }
