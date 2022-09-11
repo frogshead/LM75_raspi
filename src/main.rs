7f14af467c04ea28deeefb3c1f189e5c4eac47f1
Mikko Viitamäki <mikko.viitamaki+github@gmail.com>
2022-09-11 13:57:18 +0300

    use nanoseconds for timestamp

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate linux_embedded_hal as hal;
 extern crate lm75;
 
-use std::{time::SystemTime, thread::sleep_ms};
+use std::{time::SystemTime, thread::sleep, time::Duration};
 
 
 use hal::I2cdev;
@@ -13,11 +13,11 @@ fn main() {
 
     loop {
         let temperature = sensor.read_temperature().unwrap();
-        let unix_time = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
+        let unix_time = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_nanos();
         // printout as influxdb line protocol
         // https://docs.influxdata.com/influxdb/v1.7/write_protocols/line_protocol_reference/
         println!("lm75_measurement,name=lm75_kauhajoki temperature={} {}", temperature, unix_time);
-        sleep_ms(1000* 10); //sleep 10s
+        sleep( Duration::from_secs(10)); //sleep 10s
         
     }
        
