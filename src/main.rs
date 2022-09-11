f82ebd5d0eef268ec05f9ac88de435815173104d
Mikko Viitamäki <mikko.viitamaki+github@gmail.com>
2022-09-11 11:28:48 +0300

    use line protocol

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,19 @@
 extern crate linux_embedded_hal as hal;
 extern crate lm75;
 
-use std::fs::OpenOptions;
+use std::time::SystemTime;
 
-use chrono::prelude::*;
-use csv::WriterBuilder;
-use serde::Serialize;
 
-#[derive(Debug, Serialize)]
-struct Temperature {
-    temperature: f32,
-    date_time: String,
-}
 use hal::I2cdev;
-use lm75::{Lm75, SlaveAddr};
+use lm75::{Lm75, Address};
 fn main() {
     let dev = I2cdev::new("/dev/i2c-1").unwrap();
-    let address = SlaveAddr::default();
+    let address = Address::default();
     let mut sensor = Lm75::new(dev, address);
     let temperature = sensor.read_temperature().unwrap();
-    // println!("Temperature:  {}", temperature);
-    let file = OpenOptions::new()
-        .write(true)
-        .append(true)
-        .open("/home/mikko/temperatures.csv")
-        .unwrap();
-    let mut writer = WriterBuilder::new().has_headers(false).from_writer(file); // Writer::from_writer(file);
-    let dt = Local::now().to_string();
-    let temp = Temperature {
-        temperature,
-        date_time: dt,
-    };
-    writer.serialize(temp).unwrap();
+    let unix_time = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
+    // printout as influxdb line protocol
+    // https://docs.influxdata.com/influxdb/v1.7/write_protocols/line_protocol_reference/
+    println!("lm75_measurement, name=lm75_kauhajoki temperature={} {}", temperature, unix_time);
+       
 }
