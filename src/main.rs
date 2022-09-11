13362199da6f9d990dbb2655c62311ee64215831
Mikko Viitamäki <mikko.viitamaki+github@gmail.com>
2022-09-11 12:39:14 +0300

    run forever

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate linux_embedded_hal as hal;
 extern crate lm75;
 
-use std::time::SystemTime;
+use std::{time::SystemTime, thread::sleep_ms};
 
 
 use hal::I2cdev;
@@ -10,10 +10,15 @@ fn main() {
     let dev = I2cdev::new("/dev/i2c-1").unwrap();
     let address = Address::default();
     let mut sensor = Lm75::new(dev, address);
-    let temperature = sensor.read_temperature().unwrap();
-    let unix_time = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
-    // printout as influxdb line protocol
-    // https://docs.influxdata.com/influxdb/v1.7/write_protocols/line_protocol_reference/
-    println!("lm75_measurement, name=lm75_kauhajoki temperature={} {}", temperature, unix_time);
+
+    loop {
+        let temperature = sensor.read_temperature().unwrap();
+        let unix_time = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
+        // printout as influxdb line protocol
+        // https://docs.influxdata.com/influxdb/v1.7/write_protocols/line_protocol_reference/
+        println!("lm75_measurement, name=lm75_kauhajoki temperature={} {}", temperature, unix_time);
+        sleep_ms(1000* 10); //sleep 10s
+        
+    }
        
 }
